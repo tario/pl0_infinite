addb51d110b5f8bea0e6d54386bc16b68fabceb1
Dario Seminara <darios3@gmail.com>
2015-08-22 13:24:56 -0300

    Implemented first spec for scanner

--- spec/test.scanner.js
+++ spec/test.scanner.js
@@ -1,9 +1,40 @@
 describe("Scanner", function() {
   beforeEach(function() {
-    this.scanner = PL0Infinite.defaultScanner();
+    var tokens = [];
+    this.tokens = tokens;
+    this.scanner = new PL0Infinite.DefaultScanner({}, function(token) {
+      tokens.push(token);
+    });
   });
-  
+
   it ("should have scan function", function(){
     expect(typeof this.scanner.scan).to.be("function");
   });
+
+
+  var testParse = function(text, tokens) {
+    describe("when scan text '" + text + "'", function() {
+      beforeEach(function() {
+        this.scanner.scan(text);
+      });
+
+      tokens.forEach(function(token, index) {
+        describe("token " + index, function() {
+          it ("should have type " + token.type, function(){
+            expect(this.tokens[index].type).to.be(token.type);
+          });
+
+          if (token.value) {
+            it ("should have value " + token.value, function(){
+              expect(this.tokens[index].value).to.be(token.value);
+            });
+          }
+        });
+      });
+    });
+  };
+
+  testParse("PROCEDURE", [ {type: 'keyword', value: 'PROCEDURE'}, {type: 'EOF'}]);
+
+
 });
