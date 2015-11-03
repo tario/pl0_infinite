be6dd161ceb41ddd4ff407e780546995592e59ab
Dario Seminara <darios3@gmail.com>
2015-11-03 01:24:16 -0300

    uploaded missing asm.js

---
+++ src/pl0_infinite/asm.js
@@ -0,0 +1,33 @@
+window.Asm = (function() {
+  var asm = function() {
+    this.result = new Uint8Array(512);
+    this.nextPosition = 0;
+  };
+
+  asm.prototype.pushByte = function(value) {
+    this.result[this.nextPosition] = value;
+    this.nextPosition++;
+
+    if (this.nextPosition >= this.result.length) {
+      var oldArray = this.result;
+      this.result = new Uint8Array(this.result.length*2);
+      this.result.set(oldArray,0);
+    }    
+  };
+
+  asm.prototype.pushDword = function(value) {
+    this.pushByte(value & 0xFF);
+    this.pushByte((value>>8) & 0xFF);
+    this.pushByte((value>>16) & 0xFF);
+    this.pushByte((value>>24) & 0xFF);
+  };
+
+  asm.process = function(f) {
+    var a = new Asm();
+    f(a);
+
+    return a.result.slice(0,a.nextPosition);
+  };
+
+  return asm;
+})();
